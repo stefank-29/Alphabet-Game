f60ef1b13d7f71caec8feba41232f5cac34fe3ea
stefank-29 <stefankaraferovic@gmail.com>
2021-07-12 19:55:52 +0200

    score display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const GameStyles = styled.div`
+    height: 100vh;
+    width: 100vw;
+    background-color: #e6e6ff;
+    display: flex;
+    flex-direction: column;
+`;
+
+const HeaderStyles = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .score {
+        display: flex;
+        flex-direction: column;
+        .label {
+            color: #a3a3c2;
+        }
+        .hit {
+            color: #2eb82e;
+        }
+        .miss {
+            color: #b30000;
+        }
+        .left {
+            color: #5c5cd6;
+        }
+    }
+`;
+
+const DisplayStyles = styled.div`
+    .start-btn {
+        padding: 1.2rem 1.5rem;
+    }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [hitCount, setHitCount] = useState(0);
+    const [missCount, setMissCount] = useState(0);
+    const [leftCount, setLeftCount] = useState(26);
+
+    return (
+        <GameStyles>
+            <HeaderStyles>
+                <DisplayStyles>
+                    <button className="start-btn"></button>
+                </DisplayStyles>
+                <div className="score">
+                    <div className="label">Score</div>
+                    <div className="hit">
+                        <span>Hit: </span>
+                        <span>{hitCount}</span>
+                    </div>
+                    <div className="miss">
+                        <span>Miss: </span>
+                        <span>{missCount}</span>
+                    </div>
+                    <div className="left">
+                        <span>Left: </span>
+                        <span>{leftCount}</span>
+                    </div>
+                </div>
+            </HeaderStyles>
+        </GameStyles>
+    );
 }
 
 export default App;
